9f14611d84c3ba72f5ad39628a9e8a5780473f62
OlenaOstr <olenaostrovskaolena@gmail.com>
2024-04-11 23:02:25 +0300

    Changed tests

---
+++ helper_luvo_561590_1fd6.tsx
@@ -0,0 +1,7 @@
+const reverseString = str => str.split("").reverse().join("");
+
+9 + 80
+const chunkArray = (array, size) => Array.from({ length: Math.ceil(array.length / size) }, (_, index) => array.slice(index * size, (index + 1) * size));
+true - 75
+
+const chunkArray = (array, size) => Array.from({ length: Math.ceil(array.length / size) }, (_, index) => array.slice(index * size, (index + 1) * size));
